ec59419fc179afa564da2752dc6027e54e79751a
Shuo Han <shan50@jhu.edu>
2021-05-14 17:03:25 -0400

    Refactor resize in numpy

---
+++ resize/abstract.py
@@ -0,0 +1,115 @@
+import numpy as np
+
+
+class Resize:
+    """Resizes the image with sampling steps dx, dy, and dz.
+
+    Same FOV mode:
+        
+        |   x   |   x   |   x   |
+        | x | x | x | x | x | x |
+
+    Align first point mode:
+        
+        |   x   |   x   |   x   |
+          | x | x | x | x | x |
+
+    """
+    def __init__(self, image, dxyz, same_fov=True):
+        self.image = image
+        self.dxyz = dxyz
+        self.same_fov = same_fov
+
+        self._old_shape = self.image.shape
+        self._result = None
+        self._coords = None
+
+    def resize(self):
+        """Resizes the image."""
+        self._calc_sampling_coords()
+        self._format_coords()
+        self._resize()
+
+    @property
+    def result(self):
+        """Returns the interpolated result."""
+        return self._result
+
+    @property
+    def coords(self):
+        """Returns the sampling coordinates."""
+        return self._coords
+
+    def _calc_sampling_coords(self):
+        if self.same_fov:
+            self._old_fov = self._calc_old_fov_same_fov()
+            self._new_shape = self._calc_new_shape_same_fov()
+            self._new_fov = self._calc_new_fov_same_fov()
+            self._coords = self._calc_sampling_coords_same_fov()
+        else:
+            self._old_fov = self._calc_old_fov_align_first()
+            self._new_shape = self._calc_new_shape_align_first()
+            self._new_fov = self._calc_new_fov_align_first()
+            self._coords = self._calc_sampling_coords_align_first()
+
+    def _format_coords(self):
+        raise NotImplementedError
+
+    def _resize(self):
+        raise NotImplementedError
+
+    def _calc_sampling_coords_same_fov(self):
+        coords = list()
+        for l, r, d in zip(self._new_fov[0], self._new_fov[1], self.dxyz):
+            coords.append(np.arange(l + d/2, r - d/4, d))
+        return coords
+
+    def _calc_sampling_coords_align_first(self):
+        return [np.arange(0, f + d/4, d)
+                for f, d in zip(self._new_fov, self.dxyz)]
+
+    def _calc_old_fov_same_fov(self):
+        """Calculates the FOV of the original image.
+
+        Suppose the left boundaries are at (-0.5, -0.5, -0.5), then the first
+        voxel is at (0, 0, 0). Assume the step size is 1.
+
+        """
+        step_size = 1
+        left = (-0.5, ) * len(self._old_shape)
+        right = tuple(l + s * step_size for l, s in zip(left, self._old_shape))
+        return left, right
+
+    def _calc_old_fov_align_first(self):
+        """Assumes the FOV starts from the first and ends at the last point."""
+        step_size = 1
+        fov = tuple((s - 1) * step_size for s in self._old_shape)
+        return fov
+
+    def _calc_new_fov_same_fov(self):
+        """Calculates the FOV of the resulting image.
+
+        Assume the old and new FOV have the same center, then the new FOV is
+        shifted from the old FOV by half of the size difference.
+
+        """
+        old_size = [r - l for l, r in zip(*self._old_fov)]
+        new_size = [s * d for s, d in zip(self._new_shape, self.dxyz)]
+        size_diff = [(o - n) / 2 for o, n in zip(old_size, new_size)]
+        left = tuple(l + sd for l, sd in zip(self._old_fov[0], size_diff))
+        right = tuple(r - sd for r, sd in zip(self._old_fov[1], size_diff))
+        return left, right
+
+    def _calc_new_fov_align_first(self):
+        """The new FOV also starts from the first point."""
+        fov = tuple((s - 1) * d for s, d in zip(self._new_shape, self.dxyz))
+        return fov
+
+    def _calc_new_shape_same_fov(self):
+        return tuple(int(round(s / d))
+                     for s, d in zip(self._old_shape, self.dxyz))
+
+    def _calc_new_shape_align_first(self):
+        """The largest number of points to ensure new FOV is within the old."""
+        return tuple(int(np.floor((s - 1) / d) + 1)
+                     for s, d in zip(self._old_shape, self.dxyz))
--- resize/np.py
+++ resize/np.py
@@ -4,13 +4,28 @@
 import numpy as np
 from scipy.ndimage import map_coordinates
 
-from .utils import _calc_old_fov_same_fov, _calc_old_fov_align_first
-from .utils import _calc_new_shape_same_fov, _calc_new_shape_align_first
-from .utils import _calc_new_fov_same_fov, _calc_new_fov_align_first
+from .abstract import Resize
 
 
-def resize(image, dxyz, order=3, same_fov=True, return_coords=False):
-    """Resize the image with sampling steps dx, dy, and dz.
+def resize(image, dxyz, same_fov=True, order=3, return_coords=False):
+    """Wrapper function to resize an image using numpy.
+
+    See :class:`ResizeNumpy` for more details.
+
+    Args:
+        return_coords (bool): Return sampling coordinates if ``True``.
+
+    """
+    resizer = ResizeNumpy(image, dxyz, same_fov=same_fov, order=order)
+    resizer.resize()
+    if return_coords:
+        return resizer.result, resizer.coords
+    else:
+        return resizer.result
+
+
+class ResizeNumpy(Resize):
+    """Resizes the image with sampling steps dx, dy, and dz.
 
     Same FOV mode:
         
@@ -23,74 +38,36 @@ def resize(image, dxyz, order=3, same_fov=True, return_coords=False):
           | x | x | x | x | x |
 
     Note:
-        Assum "replication" padding in the same FOV mode.
+        Assume "replication" padding in the same FOV mode.
 
     Args:
         image (numpy.ndarray): The image to resample.
         dxyz (tuple[float]): The sampling steps. Less than 1 for upsampling.
+        same_fov (bool): Keep the same FOV if possible when ``True``.
         order (int): B-spline interpolation order.
-        same_fov (bool): Keep the same FOV if possible when ``True``.
-        return_coords (bool): Return sampling coordinates if ``True``.
-
-    Returns
-    -------
-    result : numpy.ndarray
-        The resampled image.
-    coords : numpy.ndarray (optional)
-        The sampling coordiantes. See :func:`calc_sampling_coords`.
 
     """
-    coords, new_shape = calc_sampling_coords(image.shape, dxyz, same_fov)
-    result = map_coordinates(image, coords, mode='nearest', order=order)
-    result = result.reshape(new_shape)
-    if return_coords:
-        return result, coords
-    else:
-        return result
+    def __init__(self, image, dxyz, same_fov=True, order=3):
+        self.order = order
+        super().__init__(image, dxyz, same_fov)
 
+    @property
+    def coords(self):
+        """Returns the sampling coordinates.
 
-def calc_sampling_coords(shape, dxyz, same_fov=True):
-    """Calculates sampling coordinates.
+        The shape is M x N sampling coordinate array. N is the number of points
+        to sample. It is equal to product of the shape of :meth:`result`.
+        M is the point dimension (e.g., 1, 2, 3).  Each column is the
+        coordinates vector of a point.
 
-    Args:
-        shape (tuple[int]): The shape of the image to resize.
-        dxyz (tuple[float]): The sampling steps. Less than 1 for upsampling.
-        same_fov (bool): Keep the same FOV if possible when ``True``.
+        """
+        return self._coords
 
-    Returns
-    -------
-    coords : numpy.ndarray
-        The M x N indices array. N is the number of points to sample. M is the
-        point dimension (e.g., 1, 2, 3). Each column is the coordinates vector
-        of a point.
-    new_shape : (tuple[int])
-        The shape of the resulting image. ``prod(new_shape)`` is equal to N, the
-        number of points in ``coords``.
+    def _format_coords(self):
+        self._coords = np.meshgrid(*self._coords, indexing='ij')
+        self._coords = np.array([c.flatten() for c in self._coords])
 
-    """
-    if same_fov:
-        old_fov = _calc_old_fov_same_fov(shape)
-        new_shape = _calc_new_shape_same_fov(shape, dxyz)
-        new_fov = _calc_new_fov_same_fov(old_fov, new_shape, dxyz)
-        coords = _calc_sampling_coords_same_fov(new_fov, dxyz)
-    else:
-        old_fov = _calc_old_fov_align_first(shape)
-        new_shape = _calc_new_shape_align_first(shape, dxyz)
-        new_fov = _calc_new_fov_align_first(new_shape, dxyz)
-        coords = _calc_sampling_coords_align_first(new_fov, dxyz)
-    return coords, new_shape
-
-
-def _calc_sampling_coords_same_fov(new_fov, dxyz):
-    indices = [np.arange(l + d/2, r - d/4, d)
-               for l, r, d in zip(new_fov[0], new_fov[1], dxyz)]
-    grid = np.meshgrid(*indices, indexing='ij')
-    grid = np.array([g.flatten() for g in grid])
-    return grid
-
-
-def _calc_sampling_coords_align_first(new_fov, dxyz):
-    indices = [np.arange(0, f + d/4, d) for f, d in zip(new_fov, dxyz)]
-    grid = np.meshgrid(*indices, indexing='ij')
-    grid = [g.flatten() for g in grid]
-    return np.array(grid)
+    def _resize(self):
+        self._result = map_coordinates(self.image, self.coords,
+                                       mode='nearest', order=self.order)
+        self._result = self._result.reshape(self._new_shape)
