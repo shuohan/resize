37eab4ce7fb9c96e131cddd339f10a36f4a9ec58
Shuo Han <shan50@jhu.edu>
2021-06-14 17:09:07 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from glob import glob
+from setuptools import setup, find_packages
+
+dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(dir, 'README.md')) as f:
+    long_description = f.read()
+
+setup(name='resize',
+      version='0.1.1',
+      author='Shuo Han',
+      description='Resize an image with correct sampling coordinates.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author_email='shuo_han@outlook.com',
+      url='https://github.com/shuohan/resize',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['numpy'],
+      extras_require={'scipy': 'scipy', 'pytorch': 'torch>=1.6.0'},
+      python_requires='>=3.7',
+      include_package_data=True,
+      classifiers=['Programming Language :: Python :: 3',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent'])
